3044f1e957219fd7032ee5856deea7886cd222e5
Green-grape <yhkim13729@gmail.com>
2022-02-18 06:25:32 +0000

    meeting room

--- APSS2/dfs/meeting_room.cpp
+++ APSS2/dfs/meeting_room.cpp
@@ -6,8 +6,17 @@
 
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
+
+vector<int> sccId; //정점들의 sccId
+vector<int> discovered;//정점들의 발견순서
+stack<int> s;
+int sccCounter,vertexCounter;
+
+vector<int> getPossibleMeetingTimes(const vector<pair<int,int>>);
 
 int main(){
     int testCase;
@@ -23,7 +32,16 @@ int main(){
             meetingTimes[2*j]=make_pair(s0,e0);
             meetingTimes[2*j+1]=make_pair(s1,e1);
         }
+        vector<int> res=getPossibleMeetingTimes(meetingTimes);
+        if(res.size()==0) cout << "IMPOSSIBLE"<<"\n";
+        else{
+            cout <<"POSSIBLE"<<"\n";
+            for(int i=0;i<res.size();i++){
+                if(res[i]) cout << meetingTimes[i].first <<" "<<meetingTimes[i].second<<"\n";
+            }
+        }
     }
+    return 0;
 }
 
 bool isJoint(pair<int,int> a, pair<int,int> b){
@@ -33,6 +51,7 @@ bool isJoint(pair<int,int> a, pair<int,int> b){
 /*
 주간회의와 월간회의 둘 중 '하나'만 한다는 것에 집중하면
 !주간회의 시행->월간회의 시행 or !월간회의 시행->주간회의 시행으로 함의 그래프를 생성할 수 있다.
++ 겹치는 것을 고려하여 그래프를 짤 수 있다.
 */  
 vector<vector<int>> makeGraph(const vector<pair<int,int>>& meetingTimes){ 
     vector<vector<int>> adj=vector<vector<int>>(meetingTimes.size()*2,vector<int>());
@@ -45,11 +64,74 @@ vector<vector<int>> makeGraph(const vector<pair<int,int>>& meetingTimes){
     }
     //시간이 겹칠 경우
     for(int i=0;i<meetingTimes.size();i++){
-        int j=i+1;
-        if(isJoint(meetingTimes[i],meetingTimes[j])){
-            adj[2*i].push_back(2*j+1);
-            adj[2*j].push_back(2*i+1);
+        for(int j=0;j<i;j++){
+            if(isJoint(meetingTimes[i],meetingTimes[j])){
+                adj[2*i].push_back(2*j+1);
+                adj[2*j].push_back(2*i+1);
+            }
         }
     }
     return adj;
+}
+
+//반환값:vertex에서 갈수 있는 정점중에서 가장 순서가 빠른것
+int dfs(const int vertex, const vector<vector<int>> adj){
+    int ret=discovered[vertex]=vertexCounter++;
+    s.push(vertex);
+    for(int i=0;i<adj[vertex].size();i++){
+        int next=adj[vertex][i];
+        if(discovered[next]==-1){
+            ret=min(ret,dfs(next,adj));
+        }else if(sccId[next]==-1){
+            ret=min(ret,discovered[next]);
+        }
+    }
+    if(ret==discovered[vertex]){
+        while(true){
+            int t=s.top();
+            s.pop();
+            sccId[t]=sccCounter;
+            if(t==vertex) break;
+        }
+        ++sccCounter;
+    }
+    return ret;
+}
+
+vector<int> tarjanSCC(const vector<vector<int>> adj){
+    sccId=discovered=vector<int>(adj.size(),-1);
+    sccCounter=vertexCounter=0;
+    for(int i=1;i<adj.size();i++){
+        if(discovered[i]==-1) dfs(i,adj);
+    }
+    return sccId;
+}
+
+vector<int> getPossibleMeetingTimes(const vector<pair<int,int>> meetingTimes){
+    const vector<vector<int>> adj=makeGraph(meetingTimes);
+    const vector<int> sccId=tarjanSCC(adj);
+
+    //답이 존재x
+    for(int i=0;i<sccId.size();i+=2){
+        if(sccId[i]==sccId[i+1]){ //같은 scc에 존재한다는 것은 둘다 월간,주간회의가 모두 참 이거나 거짓인 경우이다.
+            return vector<int>();
+        }
+    }
+    
+    //답이 존재
+    //sccId의 역순이 위상 정렬이라는 것을 이용한다.
+    vector<pair<int,int>> order;
+    for(int i=0;i<sccId.size();i++){
+        order.push_back(make_pair(-sccId[i],i));
+    }
+    sort(order.begin(),order.end());
+    
+    vector<int> ret(sccId.size()/2,-1);
+    for(int i=0;i<order.size();i++){
+        const int meetingTimeIdx=order[i].second/2;
+        if(ret[meetingTimeIdx]!=-1) continue;
+        // 먼저 나온 것들 이 ~이다. 
+        ret[meetingTimeIdx]=!(order[i].second%2==0);
+    }
+    return ret;
 }
